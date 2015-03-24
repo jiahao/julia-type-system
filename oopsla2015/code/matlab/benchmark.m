ef571430d16d4311793edc1f41c039fe387192a3
Jiahao Chen <jiahao@mit.edu>
2015-03-24 15:33:27 -0400

    Add Julia benchmark code.
    Also, Julia benchmark codes write timings to CSV file in data/lu

--- oopsla2015/code/matlab/benchmark.m
+++ oopsla2015/code/matlab/benchmark.m
@@ -9,3 +9,4 @@ for n = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
     end
     timings = [timings; [n t' min(t) mean(t) std(t)]]
 end
+dlmwrite('../../data/lu/matlab.csv', timings, 'precision', 15)
