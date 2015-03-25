11daa90c2b19a2fd883285a9d15172c60cf1e03f
Andreas Noack <andreasnoackjensen@gmail.com>
2015-03-25 12:42:02 -0400

    Reverse loop order in idxmaxabs and use four spaces consistently

--- oopsla2015/code/julia/lucp-opt.jl
+++ oopsla2015/code/julia/lucp-opt.jl
@@ -1,38 +1,38 @@
-x ↔ y = for i=1:length(x) #Define swap function
-  x[i], y[i] = y[i], x[i]
+x ↔ y = for i = 1:length(x) #Define swap function
+    x[i], y[i] = y[i], x[i]
 end
 
 function idxmaxabs(A, r, s)
     μ = λ = r[1]; themax = abs(A[r[1], r[1]])
-    for i in r, j in s
-        if abs(A[i, j]) > themax
-            μ, λ, themax = i, j, A[i, j]
+    for j in s, i in r
+        Aij = A[i,j]
+        if abs(Aij) > themax
+            μ, λ, themax = i, j, Aij
         end
     end
     return μ, λ
 end
 
 function lucompletepiv!(A)
-  n=size(A, 1)
-  rowpiv=zeros(Int, n-1)
-  colpiv=zeros(Int, n-1)
-  for k=1:n-1
-    μ, λ = idxmaxabs(A, k:n, k:n)
-    rowpiv[k] = μ
-    A[k, 1:n] ↔ A[μ, 1:n]
-    colpiv[k] = λ
-    A[1:n, k] ↔ A[1:n, λ]
-    if A[k,k] ≠ 0
-      ρ = k+1:n
-      scale!(1/A[k,k], sub(A, ρ, k))
-      for j in ρ
-          Akj = A[k, j]
-          for i in ρ
-              A[i, j] -= A[i, k] * Akj
-          end
-      end
+    n = size(A, 1)
+    rowpiv = zeros(Int, n - 1)
+    colpiv = zeros(Int, n - 1)
+    for k = 1:n - 1
+        μ, λ = idxmaxabs(A, k:n, k:n)
+        rowpiv[k] = μ
+        A[k, 1:n] ↔ A[μ, 1:n]
+        colpiv[k] = λ
+        A[1:n, k] ↔ A[1:n, λ]
+        if A[k,k] ≠ 0
+            ρ = k + 1:n
+            scale!(1/A[k,k], sub(A, ρ, k))
+            for j in ρ
+                Akj = A[k, j]
+                for i in ρ
+                    A[i, j] -= A[i, k] * Akj
+                end
+            end
+        end
     end
-  end
-  return (A, rowpiv, colpiv)
+    return (A, rowpiv, colpiv)
 end
-
