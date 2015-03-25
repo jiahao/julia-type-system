0fd31bff67bf2c8dfbf3b92a664b7c29d83e7b0f
Jiahao Chen <jiahao@mit.edu>
2015-03-25 02:40:02 -0400

    drop numba max size to 2000

--- oopsla2015/code/python/benchmark-numba.py
+++ oopsla2015/code/python/benchmark-numba.py
@@ -6,7 +6,7 @@ import numba
 exec(open("./lucompletepiv-numba.py").read())
 
 timings = []
-for n in [10, 20, 50, 100, 200, 500, 1000, 2000, 5000]:
+for n in [10, 20, 50, 100, 200, 500, 1000, 2000]:
     A = np.random.randn(n,n)
     t = np.ones(5)
     for i in range(0,5):
