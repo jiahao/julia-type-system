fb065661b9290d464df14ea9c17ae4257b83566b
Jake Bolewski <jakebolewski@gmail.com>
2015-03-24 20:32:13 -0400

    fix python benchmark code for Python 3.4

--- oopsla2015/code/python/benchmark.py
+++ oopsla2015/code/python/benchmark.py
@@ -2,18 +2,18 @@ import csv
 import timeit
 import numpy as np
 
-execfile("./lucompletepiv.py")
+exec(open("./lucompletepiv.py").read())
 
 timings = []
 for n in [10, 20, 50, 100, 200, 500, 1000, 2000, 5000]:
-    A = np.rand.randn(n,n)
-    t = np.zeros(5)
+    A = np.random.randn(n,n)
+    t = np.ones(5)
     for i in range(1,5):
-        t[i] = timeit.timeit(lucompletepiv(A))
+        tic = time.time()
+        lucompletepiv(A)
+        t[i] = time.time() - tic # time in seconds
     timings.append((n, t[0], t[1], t[2], t[3], t[4], t.min(), t.mean(), t.std()))
 
-
-with open("benchmark.csv", "wb") as fh:
+with open("benchmark.csv", "w", newline="") as fh:
     benchwriter = csv.writer(fh)
-    for t in timings:
-        benchwriter.writerow(t)
+    benchwriter.writerows(timings)
